732ddaaef6d6fc224da12bb71d461324f46df2a5
ninievy <virginiemirkov@gmail.com>
2019-03-28 08:17:25 +0100

    debut deleteGuestGroup

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -198,4 +198,60 @@ class PersonController extends AbstractController
         $response = new JsonResponse($data, 200);       
         return $response;
     }
+
+    /**
+     * @Route("/brides/guests/delete/{id}", name="deleteGuestGroup", requirements={"id"="\d+"}, methods={"DELETE"})
+     */
+    public function deleteGuestGroup(GuestGroupRepository $guestGroupRepository, PersonRepository $personRepository, $id, Request $request)
+    {
+        // $guestGroup = $personRepository->findByGuestGroup($id);
+        $guestGroup = $guestGroupRepository->find($id);
+
+        if (!$guestGroup){
+            $message = 'Le guestGroupId n\'existe pas';
+            $response = new Response($message, 404);
+            $response->headers->set('Content-Type', 'application/json');
+           
+            return $response;
+        }
+
+        dd($guestGroup->getPeople());
+        //je récupère les données du front dans l'objet request.
+        $content = $request->getContent();
+        $contentDecode = json_decode($content);
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        //edit email 
+        if ($guestGroup->getId() === $contentDecode->group->id){
+            $guestGroup->setEmail($contentDecode->group->email);
+            $entityManager->persist($guestGroup);
+        }   
+        
+
+        //edit persons
+        foreach ($contentDecode->group->people as $person){
+            $personBdd = $personRepository->find($person->id);
+            $personBdd->setFirstname($person->firstname);
+            $personBdd->setLastname($person->lastname);
+            $personBdd->setAttendance($person->attendance);
+            $entityManager->persist($personBdd);
+        }
+        
+        
+        
+        // dd($guestGroup);
+
+        $entityManager->flush();
+        $guestGroupArray = $guestGroupRepository->findByGuestGroupQueryBuilder($id);
+        
+        $data = 
+            [
+                'group' => $guestGroupArray
+            ]
+        ;
+
+        $response = new JsonResponse($data, 200);       
+        return $response;
+    }
 }
