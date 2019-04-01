5a5757043f9f2fe608ef81b6a8e64ee033da1c46
ninievy <virginiemirkov@gmail.com>
2019-04-01 14:14:04 +0200

    edit table ok

--- src/Controller/ReceptionTableController.php
+++ src/Controller/ReceptionTableController.php
@@ -72,7 +72,7 @@ class ReceptionTableController extends AbstractController
         return $response;
     }
 
-     /**
+    /**
      * @Route("new", name="new", methods={"POST"})
      */
     public function new(Request $request, UserRepository $userRepository, ReceptionTableRepository $receptionTableRepository, EntityManagerInterface $em)
@@ -95,6 +95,53 @@ class ReceptionTableController extends AbstractController
 
         // dd($oneTable);
         $message = 'La table a bien été ajoutée.';
+        $response = new JsonResponse($message, 200);
+        
+        return $response;
+    }
+
+    /**
+     * @Route("edit", name="edit", methods={"POST"})
+     */
+    public function edit(Request $request, UserRepository $userRepository, ReceptionTableRepository $receptionTableRepository, EntityManagerInterface $em)
+    {
+        //je récupère les données du front dans l'objet request.
+        $content = $request->getContent();
+        $contentDecode = json_decode($content);
+
+        // récupération du wedding correspondant au user grâce à AuthenticatedListener
+        $userWedding = $userRepository->findOneBy(['email' => $request->attributes->get('userEmail')])->getWedding();
+
+        $table = $receptionTableRepository->find($contentDecode->id);
+
+        if (!$table){
+            $message = 'Il n\'y a pas de table avec l\'id correspondant.';
+                
+            $response = new JsonResponse($message, 400);
+       
+            return $response;
+
+        } elseif($userWedding->getId() != $receptionTableRepository->find($contentDecode->id)->getWedding()->getId()){
+            $message = 'L\'id de la table n\'appartient pas au mariage du user connecté';
+                
+            $response = new JsonResponse($message, 400);
+       
+            return $response;
+        }
+
+        if ($contentDecode->name){
+            $table->setName($contentDecode->name);
+        }
+        if ($contentDecode->totalSeats){
+            $table->setTotalSeats($contentDecode->totalSeats);
+        }
+
+        $em->persist($table);
+        $em->flush();
+        
+        $tableId = $table->getId();
+        // dd($oneTable);
+        $message = "La table avec id : $tableId a bien été modifiée.";
         $response = new JsonResponse($message, 200);
         
         return $response;
